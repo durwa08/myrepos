70fd5e96b68a141be1d3b5156099ff2dc4f880d0
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-03-20 07:43:41 +0530

    Fixed name

---
+++ DurwaPahariya_FrontEnd/Js/DurwaPahariya_student_analyzer.js
@@ -0,0 +1,35 @@
+const students = [
+  {
+    name: "Lalit",
+    marks: [
+      { subject: "Math", score: 78 },
+      { subject: "English", score: 82 },
+      { subject: "Science", score: 74 },
+      { subject: "History", score: 69 },
+      { subject: "Computer", score: 88 }
+    ],
+    attendance: 82
+  },
+  {
+    name: "Rahul",
+    marks: [
+      { subject: "Math", score: 90 },
+      { subject: "English", score: 85 },
+      { subject: "Science", score: 80 },
+      { subject: "History", score: 76 },
+      { subject: "Computer", score: 92 }
+    ],
+    attendance: 91
+  },
+  {
+    name: "Aman",
+    marks: [
+      { subject: "Math", score: 35 },
+      { subject: "English", score: 60 },
+      { subject: "Science", score: 55 },
+      { subject: "History", score: 50 },
+      { subject: "Computer", score: 65 }
+    ],
+    attendance: 70
+  }
+];
